7f613efae1caf89047604b9a410addb739ad856a
fzzs <1030663193@qq.com>
2017-05-08 13:41:25 +0800

    中奖名单列表展示

--- Application/Home/Controller/BicycleJourneyZJController.class.php
+++ Application/Home/Controller/BicycleJourneyZJController.class.php
@@ -97,9 +97,8 @@ class BicycleJourneyZJController extends Controller{
      * @param int $pageNo
      * @param int $pageSize
      * @param string $user
-     * @param int $recid
      */
-    public function SearchUserConsume($pageNo=1,$pageSize=10,$user=null,$recid=null){
+    public function SearchUserConsume($pageNo=1,$pageSize=10,$user=null){
         $query = array();//数组作为查询条件
         if($user != "" && $user !=null){
             $query["u_account"] = array("LIKE","%$user%");
@@ -132,6 +131,60 @@ class BicycleJourneyZJController extends Controller{
         $page = array("total"=>$total,"rows"=>$rows,"pageNo"=>$pageNo,"pageSize"=>$pageSize);
         $this->assign("page",$page);
         $this->display("ZJ/loadPrizeList");
+    }
+    /**
+     * 根据奖品名搜索奖品并分页展示
+     * @param number $pageNo
+     * @param number $pageSize
+     * @param string $searchName
+     */
+    public function searchPrizeList($pageNo=1,$pageSize=10,$searchName=null){
+        //数组作为查询条件
+        $query = array();
+        //判断输入的名称是否为空进行模糊查询
+        if($searchName != null && $searchName != ""){
+            $query["pr_name"] = array("LIKE","%$searchName%");
+        }
+        //查询总的数量
+        $total = $this->adminZJmodel->table("tb_prize")->count();
+        //查询当前页展示的数据
+        $rows = $this->adminZJmodel->table("tb_prize pr")->where($query)
+        ->field("pr.pr_id,pr.pr_name,pr.pr_picture,pr.pr_integral,pr.pr_cost")
+        ->page($pageNo,$pageSize)->select();
+        //数组
+        $page = array("total"=>$total,"rows"=>$rows,"pageNo"=>$pageNo,"pageSize"=>$pageSize,"searchName"=>$searchName);
+        $this->assign("page",$page);
+        $this->display("ZJ/loadPrizeList");
+    }
+    /**
+     * 新增奖品
+     * @param string $prizeName
+     * @param string $picture
+     * @param int $integral
+     * @param int $cost
+     */
+    public function addPrizeInfo($prizeName,$picture,$integral,$cost){
+        //奖品表中插入数据
+        $data = array("pr_name"=>$prizeName,"pr_picture"=>$picture,"pr_integral"=>$integral,"pr_cost"=>$cost);
+        $this->adminZJmodel->table("tb_prize")->field("pr_name,pr_picture,pr_integral,pr_cost")->add($data);
+        //返回列表
+        $this->loadPrizeList();
+    }
+    /**
+     * 同步加载并返回中奖信息列表
+     * @param number $pageNo
+     * @param number $pageSize
+     */
+    public function loadWinnersList($pageNo=1,$pageSize=10){
+        //查询总数量
+        $total = $this->adminZJmodel->table("tb_win")->count();
+        //查询当前页展示的数据
+        $rows = $this->adminZJmodel->table("tb_win w")->join("tb_user u on u.u_id=w.u_id")->join("tb_prize pr on pr.pr_id=w.pr_id")
+        ->field("w.wi_id,u.u_account,pr.pr_name,w.wi_time")->select();
+        $page = array("pageNo"=>$pageNo,"pageSize"=>$pageSize,"total"=>$total,"rows"=>$rows);
+        //返回列表页面
+        $this->assign("page",$page);
+        $this->display("ZJ/loadWinnersList");
     }
     
 }
